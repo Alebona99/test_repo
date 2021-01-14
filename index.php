a499aa8bfc7d83414dfe6b415e45e03f0c145e7d
Alessio <academy@infocube.it>
2021-01-14 16:35:55 +0100

    Monolog commit

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
+require_once(__DIR__.'/vendor/autoload.php');
+use Monolog\Logger;
+
+$logger = new Logger ('db');
+$logger->info('Log Info');
+
 $link = mysqli_connect('db', 'root', 'password');
 
 if (!$link){
+    $logger->warning('This is a log error');
     echo "Error: impossibile connettersi al db".PHP_EOL;
     echo "Debugging errno".mysqli_connect_errno().PHP_EOL;
     echo "Debugging error".mysqli_connect_error().PHP_EOL;
@@ -11,6 +18,5 @@ echo "Connessione al databse riuscita".PHP_EOL;
 echo "Informazioni Host: ".mysqli_get_host_info($link).PHP_EOL;
 
 mysqli_close($link);
-
 ?>
 

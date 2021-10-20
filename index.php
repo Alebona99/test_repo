a01024f7d9f25b2ab8fd826e14596dc2a5d1162c
Alebona99 <77329587+Alebona99@users.noreply.github.com>
2021-10-20 14:38:35 +0200

    Update index.php
    111

--- index.php
+++ index.php
@@ -38,6 +38,7 @@ else if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
 else if ($_SERVER["REQUEST_METHOD"=="DELETE"]){
     echo "Hai mandato una richiesta DELETE";
+    echo "Hai mandato una richiesta merge";
     echo $id.$product;
     exit;
 }
